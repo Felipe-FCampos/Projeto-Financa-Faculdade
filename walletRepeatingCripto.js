4e950abb5b4912e84f108b19ffc1774a9a73da86
Felipe-FCampos <felipe.fcampos7@gmail.com>
2023-12-03 21:57:01 -0300

    criptopage added

--- walletRepeatingCripto.js
+++
@@ -1,7 +0,0 @@
-function addCripto(){
-    var cripto = document.createElement('div');
-
-    cripto.textContent = 'Nome: Idade:';
-
-    document.getElementsByClassName('wallet').appendChild(cripto)
-}
