f558df63f9af51cc725b438c6142431cb7098b70
Felipe-FCampos <felipe.fcampos7@gmail.com>
2023-12-05 18:03:47 -0300

    v1.2.1

--- index.js
+++ index.js
@@ -22,9 +22,6 @@ function validateLogin(){
     let name = document.getElementById("name").value;
     let password = document.getElementById("password").value;
 
-    //let savedName = 'Felipe';
-    //let savePassword = '0106';
-
     if(name === name && password === password){
       window.location.href = 'home.html?name=' + encodeURIComponent(name);
     } else {
