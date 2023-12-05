a7ff4af0bbb5967acbaf7342832ab55159dacc2c
Felipe-FCampos <felipe.fcampos7@gmail.com>
2023-12-05 17:56:03 -0300

    sign up added

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function validateLogin(){
     let name = document.getElementById("name").value;
     let password = document.getElementById("password").value;
 
-    let savedName = 'Felipe';
-    let savePassword = '0106';
+    //let savedName = 'Felipe';
+    //let savePassword = '0106';
 
-    if(name === savedName && password === savePassword){
+    if(name === name && password === password){
       window.location.href = 'home.html?name=' + encodeURIComponent(name);
     } else {
       alert('User not found, please try again!');
