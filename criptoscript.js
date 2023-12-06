0e3d8a207d6c2051aca0bed16ebf22c127fffc64
EA - Felipe Fernandes Campos <felipe_campos@educadventista.org>
2023-12-05 21:07:58 -0300

    v1.2.2

--- criptoscript.js
+++ criptoscript.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Função para atualizar o preço de uma criptomoeda
-    const updateCryptoPrice = (cryptoId, elementId) => {
-      fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptoId}&vs_currencies=brl`)
-        .then(response => response.json())
-        .then(data => {
-          const cryptoPrice = data[cryptoId.toLowerCase()].brl;
-          document.getElementById(elementId).innerText = `R$ ${cryptoPrice}`;
-        })
-        .catch(error => {
-          console.error(`Erro ao obter o preço do ${cryptoId}:`, error);
-          document.getElementById(elementId).innerText = `R$ ${cryptoId}`;
-        });
-    };
+  // Função para atualizar o preço de uma criptomoeda
+  const updateCryptoPrice = (cryptoId, elementId) => {
+    fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptoId}&vs_currencies=brl`)
+      .then(response => response.json())
+      .then(data => {
+        const cryptoPrice = data[cryptoId.toLowerCase()].brl;
+        document.getElementById(elementId).innerText = `R$ ${cryptoPrice}`;
+      })
+      .catch(error => {
+        console.error(`Erro ao obter o preço do ${cryptoId}:`, error);
+        document.getElementById(elementId).innerText = `R$ ${cryptoId}`;
+      });
+  };
 
-    // Atualiza os preços das criptomoedas
-    updateCryptoPrice('Bitcoin', 'Bitcoin');
-    updateCryptoPrice('Ethereum', 'Ethereum');
-    updateCryptoPrice('matic-network', 'MATIC');
-  });
-
+  // Atualiza os preços das criptomoedas
+  updateCryptoPrice('Bitcoin', 'Bitcoin');
+  updateCryptoPrice('Ethereum', 'Ethereum');
+  updateCryptoPrice('matic-network', 'MATIC');
+  updateCryptoPrice('tether', 'tether');
+  updateCryptoPrice('binancecoin', 'binancecoin');
+});
