12600fe8e3794263e70898e9d8baeffb924c8c74
Felipe-FCampos <felipe.fcampos7@gmail.com>
2023-12-07 16:39:06 -0300

    v1.2.5

--- home.js
+++ home.js
@@ -11,3 +11,13 @@ document.getElementById('userName').innerText = savedName;
 
 
 });
+
+const help = () => {
+    alert('Infelizmente estamos temporariamente sem atender ao suporte! Mesmo assim, nos envie sua solicitação ou dúvida!');
+
+    let ask = String(prompt("Em que podemos ajudar?"));
+
+    alert("Sua solicitação foi enviada com sucesso!");
+    
+    console.log(ask);
+}
